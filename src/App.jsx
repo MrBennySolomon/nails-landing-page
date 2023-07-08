f1b359bb1f6d2ec3187657e49c9f0875695571af
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-07-08 21:03:02 +0300

    drop down box

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import "./App.css";
 import { useState } from "react";
@@ -11,17 +12,25 @@ function App() {
   const [greet, setIsGreet] = useState("תודה שנרשמתם");
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [select, setSelect] = useState("");
+
+  const selectHandler = e => {
+    setSelect(e.target.value);
+  }
 
   const clickHandler = () => {
-    if (name.length > 0 && phone.length >= 9) {
+    if (name.length > 0 && phone.length >= 9 && email.length > 5) {
       setIsLoading(true);
 
-      fetch("https://64a964958b9afaf4844aa1e7.mockapi.io/users", {
+      fetch("https://64a964958b9afaf4844aa1e7.mockapi.io/data", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           name: name,
-          phone: phone
+          email: email,
+          phone: phone,
+          course: select.length === 0 ? "all included" : select
         })
       }).then((value) => {
         setIsLoading(false);
@@ -34,7 +43,6 @@ function App() {
     <div className="flex">
       <div className="video-overlay">
         <video autoPlay muted loop id="myVideo">
-          
           <source
             className="src-mobile"
             src={nailsMobile}
@@ -48,22 +56,39 @@ function App() {
             type="video/mp4"
             media="(min-width: 601px) and (max-width: 2000px)"
           />
-
         </video>
       </div>
-      <h1>עוד קורס ציפורניים מטורף של מאסטר ניילס מתחיל</h1>
-      <p></p>
-      <h1>הרשמו עכשיו</h1>
+      <h1>הרשמו עכשיו למגוון קורסים של מאסטר ניילס</h1>
+      <label htmlFor="pkg">בחרו חבילה</label>
+
+      <select onChange={selectHandler} name="pkg" id="pkg">
+        <option value="all included">חבילת הכל כלול</option>
+        <option value="build in acril">בנייה באקריל</option>
+        <option value="build in gel">בנייה בג'ל</option>
+        <option value="buissness marketing">שיווק העסק</option>
+        <option value="gel on natural">ג'ל על טבעיות</option>
+        <option value="recorded lectures">הדרכות מוקלטות</option>
+        <option value="package of products">חבילת מוצרים</option>
+        <option value="private zoom">זומים אישיים</option>
+      </select>
       {!isLoading && !isShowingGreet && (
         <div className="form">
           <input
             onChange={(e) => setName(e.target.value)}
             id="name"
+            type="text"
             placeholder="שם"
+          />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+            type="email"
+            placeholder="אימייל"
           />
           <input
             onChange={(e) => setPhone(e.target.value)}
             id="phone"
+            type="number"
             placeholder="טלפון"
           />
           <button onClick={clickHandler}>אני רוצה להצטרף לקורס</button>
