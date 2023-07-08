eeb99435aecbea2b777fc279b7050a23959a0ae9
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-07-08 16:53:56 +0300

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
+/* eslint-disable no-unused-vars */
 import './App.css'
 import { useState } from 'react';
 import nails from "./assets/nails-background.mp4";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isShowingGreet, setIsShowingGreet] = useState(false);
+  
+  const [greet, setIsGreet] = useState("תודה שנרשמתם");
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
 
   const clickHandler = () => {
-    fetch(
-      "https://nails-landing-page-default-rtdb.europe-west1.firebasedatabase.app",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        'Access-Control-Allow-Origin': '*',
-        body: JSON.stringify({
-          name: name,
-          phone: phone
-        })
-      }
-    );
+    setIsLoading; (true);
+    
+    fetch("https://64a964958b9afaf4844aa1e7.mockapi.io/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: name,
+        phone: phone
+      })
+    }).then((value) => {
+      setIsLoading(false);
+      setIsShowingGreet(true);
+    });
   }
 
   return (
@@ -30,19 +36,23 @@ function App() {
       </div>
       <h1>עוד קורס ציפורניים מטורף של מאסטר ניילס מתחיל</h1>
       <h1>הרשמו עכשיו</h1>
-      <div className="form">
-        <input
-          onChange={(e) => setName(e.target.value)}
-          id="name"
-          placeholder="שם"
-        />
-        <input
-          onChange={(e) => setPhone(e.target.value)}
-          id="phone"
-          placeholder="טלפון"
-        />
-        <button onClick={clickHandler}>אני רוצה להצטרף לקורס</button>
-      </div>
+      {!isLoading && !isShowingGreet && (
+        <div className="form">
+          <input
+            onChange={(e) => setName(e.target.value)}
+            id="name"
+            placeholder="שם"
+          />
+          <input
+            onChange={(e) => setPhone(e.target.value)}
+            id="phone"
+            placeholder="טלפון"
+          />
+          <button onClick={clickHandler}>אני רוצה להצטרף לקורס</button>
+        </div>
+      )}
+      {isLoading && <div className="loader"></div>}
+      {isShowingGreet && <h1 className="thanks">תודה שנרשמתם</h1>}
     </div>
   );
 }
