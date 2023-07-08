38549f52398e6e0c7263d20deedca2e99c1bcc93
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-07-08 16:24:22 +0300

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import './App.css'
+import { useState } from 'react';
 import nails from "./assets/nails-background.mp4";
 
 function App() {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const clickHandler = () => {
+    fetch(
+      "https://nails-landing-page-default-rtdb.europe-west1.firebasedatabase.app",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        'Access-Control-Allow-Origin': '*',
+        body: JSON.stringify({
+          name: name,
+          phone: phone
+        })
+      }
+    );
+  }
 
   return (
     <div className="flex">
@@ -13,9 +31,17 @@ function App() {
       <h1>עוד קורס ציפורניים מטורף של מאסטר ניילס מתחיל</h1>
       <h1>הרשמו עכשיו</h1>
       <div className="form">
-        <input id="name" placeholder='שם'/>
-        <input id="phone" placeholder='טלפון'/>
-        <button>אני רוצה להצטרף לקורס</button>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          id="name"
+          placeholder="שם"
+        />
+        <input
+          onChange={(e) => setPhone(e.target.value)}
+          id="phone"
+          placeholder="טלפון"
+        />
+        <button onClick={clickHandler}>אני רוצה להצטרף לקורס</button>
       </div>
     </div>
   );
