4d7a42b7333853a7df42a2acded54f0b6af4fd2f
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-07-08 21:46:55 +0300

    added details

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import nailsDesktop from "./assets/nails-background.mp4";
 import nailsMobile from "./assets/nails-background-mobile.mp4";
+import details from "./assets/pkg.jpeg";
 
 function App() {
+  const flexRef = useRef();
+  const flexImgRef = useRef();
+
   const [isLoading, setIsLoading] = useState(false);
   const [isShowingGreet, setIsShowingGreet] = useState(false);
 
@@ -14,6 +18,11 @@ function App() {
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [select, setSelect] = useState("");
+
+  const detailsHandler = () => {
+    flexImgRef.current.classList.toggle("none");
+    flexRef.current.classList.toggle("none");
+  }
 
   const selectHandler = e => {
     setSelect(e.target.value);
@@ -39,64 +48,76 @@ function App() {
     }
   };
 
+  useEffect(() => {
+    flexImgRef.current.classList.add('none');
+  }, []);
+
   return (
-    <div className="flex">
-      <div className="video-overlay">
-        <video autoPlay muted loop id="myVideo">
-          <source
-            className="src-mobile"
-            src={nailsMobile}
-            type="video/mp4"
-            media="(min-width: 250px) and (max-width: 600px)"
-          />
+    <>
+      <div ref={flexRef} className="flex">
+        <div className="video-overlay">
+          <video autoPlay muted loop id="myVideo">
+            <source
+              className="src-mobile"
+              src={nailsMobile}
+              type="video/mp4"
+              media="(min-width: 250px) and (max-width: 600px)"
+            />
 
-          <source
-            className="src-desktop"
-            src={nailsDesktop}
-            type="video/mp4"
-            media="(min-width: 601px) and (max-width: 2000px)"
-          />
-        </video>
-      </div>
-      <h1>הרשמו עכשיו למגוון קורסים של מאסטר ניילס</h1>
-      <label htmlFor="pkg">בחרו חבילה</label>
-
-      <select onChange={selectHandler} name="pkg" id="pkg">
-        <option value="all included">חבילת הכל כלול</option>
-        <option value="build in acril">בנייה באקריל</option>
-        <option value="build in gel">בנייה בג'ל</option>
-        <option value="buissness marketing">שיווק העסק</option>
-        <option value="gel on natural">ג'ל על טבעיות</option>
-        <option value="recorded lectures">הדרכות מוקלטות</option>
-        <option value="package of products">חבילת מוצרים</option>
-        <option value="private zoom">זומים אישיים</option>
-      </select>
-      {!isLoading && !isShowingGreet && (
-        <div className="form">
-          <input
-            onChange={(e) => setName(e.target.value)}
-            id="name"
-            type="text"
-            placeholder="שם"
-          />
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            id="email"
-            type="email"
-            placeholder="אימייל"
-          />
-          <input
-            onChange={(e) => setPhone(e.target.value)}
-            id="phone"
-            type="number"
-            placeholder="טלפון"
-          />
-          <button onClick={clickHandler}>אני רוצה להצטרף לחבילה</button>
+            <source
+              className="src-desktop"
+              src={nailsDesktop}
+              type="video/mp4"
+              media="(min-width: 601px) and (max-width: 2000px)"
+            />
+          </video>
         </div>
-      )}
-      {isLoading && <div className="loader"></div>}
-      {isShowingGreet && <h1 className="thanks">תודה שנרשמתם</h1>}
-    </div>
+        <h1>הרשמו עכשיו למגוון קורסים של מאסטר ניילס</h1>
+        <label htmlFor="pkg">בחרו חבילה</label>
+
+        <select onChange={selectHandler} name="pkg" id="pkg">
+          <option value="all included">חבילת הכל כלול</option>
+          <option value="build in acril">בנייה באקריל</option>
+          <option value="build in gel">בנייה בג'ל</option>
+          <option value="buissness marketing">שיווק העסק</option>
+          <option value="gel on natural">ג'ל על טבעיות</option>
+          <option value="recorded lectures">הדרכות מוקלטות</option>
+          <option value="package of products">חבילת מוצרים</option>
+          <option value="private zoom">זומים אישיים</option>
+        </select>
+        <br />
+        <a onClick={detailsHandler}>לחץ לפרטים</a>
+        {!isLoading && !isShowingGreet && (
+          <div className="form">
+            <input
+              onChange={(e) => setName(e.target.value)}
+              id="name"
+              type="text"
+              placeholder="שם"
+            />
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+              type="email"
+              placeholder="אימייל"
+            />
+            <input
+              onChange={(e) => setPhone(e.target.value)}
+              id="phone"
+              type="number"
+              placeholder="טלפון"
+            />
+            <button onClick={clickHandler}>אני רוצה להצטרף לחבילה</button>
+          </div>
+        )}
+        {isLoading && <div className="loader"></div>}
+        {isShowingGreet && <h1 className="thanks">תודה שנרשמתם</h1>}
+      </div>
+      <div ref={flexImgRef} className="flex-img">
+        <a onClick={detailsHandler}>חזור</a>
+        <img src={details} width="100%" />
+      </div>
+    </>
   );
 }
 
