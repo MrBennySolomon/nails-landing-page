0e7ddcba5902f794d8a6bba09a74ebb52013ba3f
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-07-08 15:31:04 +0300

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import nails from "./assets/nails-background.mp4";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex">
+      <div className="video-overlay">
+        <video autoPlay muted loop id="myVideo">
+          <source src={nails} type="video/mp4" />
+        </video>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1>עוד קורס ציפורניים מטורף של מאסטר ניילס מתחיל</h1>
+      <h1>הרשמו עכשיו</h1>
+      <div className="form">
+        <input id="name" placeholder='שם'/>
+        <input id="phone" placeholder='טלפון'/>
+        <button>אני רוצה להצטרף לקורס</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
